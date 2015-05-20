2555088f28ec3787aa83ee806e5e25cfe667babb
Chris Jacobs <zonk1024@gmail.com>
2015-05-20 12:29:09 -0700

    •_•

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from common_lib.users.models import User
+from index.models import Page, Comment
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Page)
+admin.site.register(Comment)
--- users/models.py
+++ users/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class User(models.Model):
     username = models.CharField()
     password = models.CharField()
+    first_name = models.CharField()
+    last_name = models.CharField()
